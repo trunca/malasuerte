6df722f188b935637585419651ef21c2d73d0119
Betacentauri <betacentauri@arcor.de>
2018-02-22 18:50:24 +0100

    Fix bug in .meta file creation
    m_length stores a pts value. pts_t is a long long and not a int.

--- lib/dvb/metaparser.h
+++ lib/dvb/metaparser.h
@@ -16,9 +16,8 @@ public:
 
 	eServiceReferenceDVB m_ref;
 	int m_data_ok, m_time_create, m_packet_size, m_scrambled;
-	pts_t  m_length;
 	std::string m_name, m_description, m_tags, m_service_data;
-	long long m_filesize;
+	long long m_filesize, m_length;
 };
 
 #endif
